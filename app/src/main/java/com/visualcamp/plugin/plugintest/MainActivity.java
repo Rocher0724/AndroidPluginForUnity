28aa56827b14d2fa776b000c1e808badd817aeee
Dean <rocher0724.dev@gmail.com>
2018-04-10 17:52:16 +0900

    일부 추가

--- app/src/main/java/com/visualcamp/plugin/plugintest/MainActivity.java
+++ app/src/main/java/com/visualcamp/plugin/plugintest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.visualcamp.plugin.plugintest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -40,14 +41,20 @@ public class MainActivity extends UnityPlayerActivity {
    *
    *  컨텍스트가 필요 없는 함수
    *
-   * 	  필요한 함수가 있는 클래스 가져오기
+   * 	  내가 필요한 함수가 있는 클래스 가져오기
    * 	  vrexPlugin = new AndroidJavaObject("camp.visual.gazereceiverplugin.MainActivity"); // Manifest 등록 ?
    *
-   * 	  함수 사용하기
-   * 	  text.text = vrexPlugin.Call<string> ("GetTextPlugin", 99);
+   * 	  함수 사용할 때 non static method는 context를 쏴줘야 실행되는 경향이 있다.
+   * 	  text.text = vrexPlugin.Call<string> ("GetTextPlugin", app, etc... );
+   *
+   * 	  static method는 context를 쏴주지않아도 된다.
+   *
+   * 	  스크립트가 들어가는 게임오브젝트 이름이 UnitySendMessage의 첫번째 인자와 같아야한다
+   * 	  아래 같은 경우 Material 이어야한다.
+   *
    *
    */
-  public static String GetTextPlugin(int number) {
+  public static String GetTextPlugin(Context context, int number) {
     Log.e("qwdqwd" , "qwdqwd GetTextPlugin");
     UnityPlayer.UnitySendMessage("Material", "onText", "4");
     return "Number is " + number;
